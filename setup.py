04abba8a758432715fd0286c560c3c8fb034d204
arvestad <arve@math.su.se>
2018-11-01 17:03:20 +0100

    Working version, but details are wrong

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
     author="Lars Arvestad",
     author_email="arve@math.su.se",
     description="Rapid identification of sequence evolution models",
-    scripts = ['bin/modelmatcher'],
+    scripts = ['bin/modelmatcher', 'bin/gen_q', 'bin/gen_freq'],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/arvestad/modelmatcher", # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  To be created!
